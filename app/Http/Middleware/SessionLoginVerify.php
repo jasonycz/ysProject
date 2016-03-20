560683d36c6c286861e356678ca85f4653dda45e
qianqiuyitong <qianqiuyitong@163.com>
2016-03-20 15:11:30 +0800

    session user id

--- app/Http/Middleware/SessionLoginVerify.php
+++ app/Http/Middleware/SessionLoginVerify.php
@@ -29,8 +29,8 @@ class SessionLoginVerify
             $sessionUser =(array)$sessionUser;
             $userId = array_key_exists('user_id',$sessionUser) ? $sessionUser['user_id']; 
             $studioId = array_key_exists('studio_id',$sessionUser) ? $sessionUser['studio_id'];
-        $request['user_id'] = $userId;
-        $request['studio_id'] = $studioId;
+        $request['user_id'] = $userId; //记录用户id
+        $request['studio_id'] = $studioId; //记录工作室id
         return $next($request);
     }
 }
