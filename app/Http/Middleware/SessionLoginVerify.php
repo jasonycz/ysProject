98e7dc9d77e2145e74ae62546ff049794c141961
qianqiuyitong <qianqiuyitong@163.com>
2016-03-20 15:07:36 +0800

    session user id

--- app/Http/Middleware/SessionLoginVerify.php
+++ app/Http/Middleware/SessionLoginVerify.php
@@ -27,9 +27,9 @@ class SessionLoginVerify
             ]);
         } 
             $sessionUser =(array)$sessionUser;
-            $userId = array_key_exists('user_id',$sessionUser) ? $sessionUser['user_id'] : $sessionUser['id'];  //有问题？？？
-            $studioId = array_key_exists('studio_id',$sessionUser) ? $sessionUser['studio_id'] : $sessionUser['studio_id'];
-        $request['suid'] = $userId;
+            $userId = array_key_exists('user_id',$sessionUser) ? $sessionUser['user_id'];  //有问题？？？
+            $studioId = array_key_exists('studio_id',$sessionUser) ? $sessionUser['studio_id'];
+        $request['user_id'] = $userId;
         $request['studio_id'] = $studioId;
         return $next($request);
     }
