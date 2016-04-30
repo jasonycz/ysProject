887e98a660eb6ee3a9b22e8c183c56cb9308fca0
Bell <bellchen131@gmail.com>
2016-05-01 07:52:48 +0800

    commit by ycz about aacesstokenfile.php

--- resources/weixin/accesstokenfile.php
+++ resources/weixin/accesstokenfile.php
@@ -1,5 +1,5 @@
 <?php 
 return array (
-  'expire_time' => 1461866016,
-  'access_token' => 'kyVQGTQ_uCfZEJRdcx2M4IM8lpeOwsyMQhd4mbIuLfqn_LeDcrkx0c3giCBYRMflLA53lgQyMPsWkkUkWUapYTS4Kzwi6PwiyOBrJCiNhIb4wkH6W51aykOSsdIYKHJNZQAfAJAVMW',
+  'expire_time' => 1462031762,
+  'access_token' => '-Mx9PHPJ8kCFZdzvc7hi3p62zx7kAk7qMP6Ohq0q5gL0mzgfedHSI5OBe7WlqryHgGzNKpasbXgcO1F94MCzlW62SvtOQETu4jJzH7kxUfoxSo-4mOdO4LURRS-H19tBQTEeAIAWZK',
 );
--- resources/weixin/ticketfile.php
+++ resources/weixin/ticketfile.php
@@ -1,5 +1,5 @@
 <?php 
 return array (
-  'expire_time' => 1461866016,
-  'jsapi_ticket' => 'sM4AOVdWfPE4DxkXGEs8VJfuYj9XPwTFz_-ttVuViNLyX-ozZGxr8yJix4jnQpq-xgqtOOahZoBNQ2mmOio-iQ',
+  'expire_time' => 1462031762,
+  'jsapi_ticket' => 'sM4AOVdWfPE4DxkXGEs8VJfuYj9XPwTFz_-ttVuViNK_4N7DxOxnoK8-c3GFt4i3aavn0TzXAy7Ez4mwSmJO2Q',
 );
